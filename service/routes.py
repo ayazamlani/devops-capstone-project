50a828d96e73c1490b842b798c60e31a3cf7b05b
ayazamlani <ayazamlani@gmail.com>
2023-01-22 19:53:13 -0500

    linting errors

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -69,6 +70,7 @@ def list_accounts():
     account_list = [account.serialize() for account in accounts]
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
+
 
 ######################################################################
 # READ AN ACCOUNT
@@ -84,6 +86,7 @@ def read_account(id):
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account [{id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
+
 
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
@@ -104,6 +107,8 @@ def update_accounts(account_id):
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
+
+    
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
